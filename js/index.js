76e70c3b9af83c7e0c839ec572bbead70dccbd56
tnga <devtnga@gmail.com>
2016-07-28 10:56:44 +0100

    modal dialog for waiting alert with loadings process

--- js/index.js
+++ js/index.js
@@ -3,8 +3,7 @@ bower.import('iui');
 bower.import('reveal.js', function (err) {
 	
 	if (err.occured) {
-		console.error('Oops it seems like `reveal.js` wasn\'t fully loaded by:'+ err.from) ;
-		return null; // interruption
+		throw new Error('Oops it seems like `reveal.js` wasn\'t fully loaded by:'+ err.from) ;
 	}
 	
 	// More info https://github.com/hakimel/reveal.js#configuration
@@ -21,9 +20,134 @@ bower.import('reveal.js', function (err) {
 	});
 });
 
+bower.ready( function (err) {
+	
+	// show the page rendering
+	setSideBarDisplay();
+	document.getElementsByClassName('reveal')[0].removeAttribute('data-hidden') ;
+	// hide sidebar after 10s
+	setTimeout( setSideBarDisplay, 10000);
+	
+	if (err.occured) {
+		var errMsg = '' ;
+		if (err.fromBrowser.length !== 0) errMsg += 'Oops it seems like: '+ err.fromBrowser.join(', ') +' occured a loading error from: browser \n';
+		if (err.fromBowerder.length !== 0) errMsg += 'Oops it seems like: '+ err.fromBowerder.join(', ') +' occured a loading error from: bowerder';
+		
+		dialogSectionContent.innerHTML = '<center style="color:red">'+ errMsg.replace('\n', '<br />') +'</center>';
+		throw new Error( errMsg ) ;
+	}
+	
+	dialogSectionContent.innerHTML = '<center style="color:green">needed modules have been successfully loaded by <b>bowerder</b></center>';
+});
+
+
+// first execution
+//----------------
+
+var dialogSectionContent = openMIDialog("<center>waiting for modules loading ...</center>") ;
+ 
+//--------------------
+// end first execution
+
 
 function setSideBarDisplay() {
 	
 	var sidebar = document.getElementById('sidebar');
 	if (sidebar.hasAttribute('data-hidden')) sidebar.removeAttribute('data-hidden'); else sidebar.setAttribute('data-hidden', true);
+}
+
+// @TODO remove this below hack when will upgrade to a next stable IUI production, considering that it will be a part functionnality
+
+/* Cette fonction définie et crée une "boite de dialogue" personnalisée (pas vraiment au sens propre).
+ * Elle permet d'afficher du texte ou le résultat d'un code ou modèle HTML.
+ * Le style appliqué à cette "boite de dialogue" est défini dans le fichier de style approprié
+ * La définition de style des éléments enfants peut se faire à partir du sélecteur ".main-fixed-block" ou "#mi-dialog-panel>section".
+ *
+ * Le code minimal HTML généré dans l'élément "body" est le suivant:
+ * <DIV class="main-fixed-block" id="mi-dialog-panel" >
+
+		<Section>
+			<button class="b-dialog-close" id="b-panel-close">X</button>
+		</Section>
+
+	</DIV>
+*/
+function openMIDialog(content, dWidth, dHeight) {
+
+	var closeButton = document.createElement("button");
+	closeButton.className = "b-dialog-close";
+	//closeButton.id = "b-panel-close";
+	closeButton.innerHTML = "X";
+
+	//le conteneur principale
+	var dialogSection = document.createElement("section");
+	dialogSection.appendChild(closeButton);
+
+	//prise en charge du contenu en fonction du type référence
+	var paragraph = document.createElement("p");
+	if (typeof content === 'string' ) {
+		paragraph.innerHTML = content;
+	} else if (content instanceof HTMLElement) {
+		paragraph.appendChild(content);
+	}
+	dialogSection.appendChild(paragraph);
+
+	if (typeof dWidth === 'number')
+		dialogSection.style.width = dWidth + "px";
+	else if (typeof dWidth === 'string')
+		dialogSection.style.width = dWidth; //exple: dWidth=300px ou 50%
+
+	if (typeof dHeight === 'number')
+		dialogSection.style.height = dHeight + "px";
+	else if (typeof dHeight === 'string')
+		dialogSection.style.height = dHeight; //exple: dHeight=300px ou 50%
+
+	//block fixe principale: conteneur global
+	var mainFixedBlock = document.createElement("div");
+	mainFixedBlock.className = "main-fixed-block mi-fixed-dialog";
+	mainFixedBlock.id = "mi-dialog-panel";
+	mainFixedBlock.appendChild(dialogSection);
+
+	document.body.style.overflow = "hidden";
+	document.body.appendChild(mainFixedBlock);
+
+	//fermeture lors du clic sur le bouton considéré
+	closeButton.onclick = function () {
+		document.body.style.overflow = "auto";
+		document.body.removeChild(mainFixedBlock);
+	}
+
+	//fermeture lors du clic sur un espace vide du conteneur global
+	mainFixedBlock.addEventListener('click', function (e) {
+
+		if (e.target.id == "mi-dialog-panel") {
+			document.body.style.overflow = "auto";
+			document.body.removeChild(mainFixedBlock);
+		};
+
+	}, false);
+
+	//centre suivant la hauteur le contenu de la boite de dialogue
+	var centerContentPosition = function () {
+
+		maxDSHeight = mainFixedBlock.offsetHeight - (mainFixedBlock.offsetHeight * 30 / 100) ;
+
+		if ( dialogSection.offsetHeight > maxDSHeight) {
+
+			dialogSection.offsetHeight = (maxDSHeight >= 100) ? maxDSHeight + "px" : "100px" ;
+			dialogSection.style.margin = "15% auto" ;
+
+		} else {
+
+			dsMargin = (mainFixedBlock.offsetHeight - dialogSection.offsetHeight) / 2 ;
+
+			dialogSection.style.margin = dsMargin + "px auto" ;
+		}
+	}
+
+	centerContentPosition() ;
+	//pour centrer suivant la hauteur le contenu de la boite de dialogue
+	window.addEventListener('resize', centerContentPosition, false);
+	
+	return paragraph ;
 }
